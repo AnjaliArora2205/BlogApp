814e1af04b90a7ef9b3e901e8d8a52fe580fe569
Anjali Sidana <66469505+AnjaliArora2205@users.noreply.github.com>
2021-04-18 14:26:25 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,10 @@
+module.exports={
+   MONGOURL:"mongodb+srv://anjali:123@cluster0.rv6v1.mongodb.net/BlogApp?retryWrites=true&w=majority"
+}
+//if(process.env.MODE==='production')
+//{
+//module.exports=require('./prod');
+//}else
+//{
+//module.exports=require('./devs');
+//}
